f43d74c66574bce23eb7ed5c764b7ecec494b645
suraksha2012 <152507298+suraksha2012@users.noreply.github.com>
2023-11-30 20:26:46 +0530

    Update streamlit_app.py
    omega,kale,egg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard boiled free range egg')
